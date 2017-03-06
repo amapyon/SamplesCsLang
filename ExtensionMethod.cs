e093d89ecd40ef5147a907162f0baa635f48b584
AMANO Masaru <github@amapyon.org>
2017-03-07 08:17:50 +0900

    拡張メソッドの練習

---
+++ ExtensionMethod.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SamplesCsLang;
+
+namespace SamplesCsLang
+{
+    // 拡張メソッド C#3.0より
+
+    public static class ExtensionsString
+    {
+        // staticクラスに、staticメソッドを宣言する
+        // 第一引数にthisと型を指定する、引数名は何でもよい
+        public static string Me(this string temp)
+        {
+            return "I'm Me.";
+        }
+
+        public static string Me(this string temp, string name)
+        {
+            return "I'm Me." + name;
+        }
+
+        public static DateTime NextDay(this DateTime temp)
+        {
+            var t = DateTime.Today;
+            return new DateTime(t.Year, t.Month, t.Day + 1);
+        }
+    }
+
+    [TestClass]
+    public class ExtensionMethod
+    {
+        [TestMethod]
+        public void TestExtensionMethod()
+        {
+            Console.WriteLine("".Me());
+            Console.WriteLine("".Me("Taro"));
+
+            Console.WriteLine(DateTime.Today.NextDay());
+        }
+
+    }
+
+}
